b85bc134a7ac3c3afff6ceda76184bff61a9d2c8
MEGAN IVEY <PRINTMEGANIVEY@GMAIL.COM>
2018-06-18 12:25:16 -0700

    starting project

---
+++ cookiestand.js
@@ -0,0 +1,56 @@
+'use strict';
+
+//NAME OF LOCATIONS
+//1st and Pike
+//SeaTac Airport
+//Seattle Center
+//Capitol Hill
+//Alki
+
+//MIN CUSTOMERS
+//23
+// 3
+// 11
+// 20
+// 2
+
+//MAX CUSTOMERS
+//65
+//24
+//38
+//38
+//16
+
+//AVG COOKIE SALE
+//6.3
+//1.2
+//3.7
+//2.3
+//4.6
+
+var firstAndPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgSale: 6.3,
+},
+
+var seaTacAirport = {
+  minCust: 3,
+  maxCust: 24,
+  avgSale: 1.2,
+},
+
+var seattle = {
+  minCust: 11,
+  maxCust: 38,
+  avgSale: 3.7,
+},
+
+
+
+var capitolHill= {
+  minCust: 20,
+  maxCust: 38,
+  avgSale: 2.3,
+},
+var alki
